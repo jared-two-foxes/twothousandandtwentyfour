b25825976242a2e42115afaeeaf87308186c4fd3
jared-two-foxes <jared-two-foxes@users.noreply.github.com>
2025-04-13 15:02:57 +1200

    vec2 visibility and meta tags

--- src/vec2.rs
+++ src/vec2.rs
@@ -1,15 +1,19 @@
 // A 2-dimensional distinct coordinate
 #[derive(Clone, Copy, PartialEq, Eq)]
-struct Vec2 {
+pub struct Vec2 {
     pub x: i32,
     pub y: i32,
 }
 
 impl Vec2 {
+  #[inline(always)]
+  #[must_use]
   pub fn new(x: i32, y: i32) -> Self {
     Self { x, y }
   }
   
+  #[inline]
+  #[must_use]
   pub fn splat(v: i32) -> Self {
     Self { x: v, y: v }
   }
